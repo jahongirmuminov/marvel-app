924e6f5c39f67829141141c7eddcd91713461b12
jahongir <muminovj707@gmail.com>
2021-10-14 20:46:56 +0300

    error removed

--- src/api.js
+++ src/api.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 export const api = () => {
     return axios.create({
-      baseURL: "http://gateway.marvel.com/v1/public/",
+      baseURL: "https://gateway.marvel.com/v1/public/",
       headers: {
         'Content-Type': 'application/json',
       },   
